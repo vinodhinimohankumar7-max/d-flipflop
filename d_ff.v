89a0d70ad8cb491e588f58e592213fb883ca0376
vinodhinimohankumar7-max <vinodhinimohankumar7@gmail.com>
2025-10-19 14:54:04 +0530

    Add files via upload

---
+++ d_ff.v
@@ -0,0 +1,11 @@
+module d_ff ( 
+    input  wire clk, rst, D, 
+    output reg  Q 
+); 
+    always @(posedge clk or posedge rst) begin 
+        if (rst) 
+            Q <= 1'b0;   // Reset 
+        else 
+            Q <= D;      // Capture D at clock edge 
+    end 
+endmodule
